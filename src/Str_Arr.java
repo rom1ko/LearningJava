186ba0f7f7461b08412942011b50e6df90e442f3
rom1ko <aron.pl@yandex.ru>
2018-04-11 20:32:36 +0300

    qwe
    qwe

---
+++ src/Str_Arr.java
@@ -0,0 +1,11 @@
+public class Str_Arr {
+    public static void main(String[] args) {
+        String[] str = new String[3];
+        str[0] = "privet";
+        str[1] = "poka";
+        str[2] = "qwe";
+        
+
+
+    }
+}
